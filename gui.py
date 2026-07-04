8fcf5445c9d54252909061a62175dafa7670052d
Sena Demirci <demircisena206@gmail.com>
2026-07-05 00:21:47 +0300

    Add gui.py - basic window

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+def main():
+    window = tk.Tk()
+    window.title("Mentor AI")
+    window.geometry("1200x700")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
