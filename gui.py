ecfb935a39048c8030706a6e83e7022647184fe8
Sena Demirci <demircisena206@gmail.com>
2026-07-16 17:04:16 +0300

    Create initial GUI layout

--- gui.py
+++ gui.py
@@ -4,3 +4,18 @@ def main():
     window.title("Mentor AI")
     window.geometry("1200x700")
 
+    top_frame = tk.Frame(window, bg="#181818")
+    top_frame.pack(fill = "x")
+    button1 = tk.Button(top_frame, text = "File")
+    button1.pack(side = "left")
+
+    main_frame = tk.Frame(window)
+    main_frame.pack()
+
+    left_frame = tk.Frame(main_frame, width= 800, height=700)
+    left_frame.pack(side = "left")
+    right_frame = tk.Frame(main_frame, width=400, height=700, bg="#dcdcdc")
+    right_frame.pack(side="right")
+
+    window.mainloop()
+main()
