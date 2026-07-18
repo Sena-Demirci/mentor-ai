a803676ca58d59a56566fb36f60bb73320b0ec98
Sena Demirci <demircisena206@gmail.com>
2026-07-18 23:14:21 +0300

    Create main content frame architecture

--- gui.py
+++ gui.py
@@ -13,9 +13,24 @@ def main():
     main_frame.pack()
 
     left_frame = tk.Frame(main_frame, width= 800, height=700)
-    left_frame.pack(side = "left")
+    left_frame.pack(side = "left" , expand=True)
     right_frame = tk.Frame(main_frame, width=400, height=700, bg="#dcdcdc")
-    right_frame.pack(side="right")
+    right_frame.pack(side="right", expand =True)
+
+    code_editor = tk.Text(left_frame, width = 100, height= 700)
+    code_editor.pack(fill = "both")
+
+    content_frame = tk.Frame(right_frame)
+    content_frame.pack(fill = "both", expand= True)
+
+    welcome_frame = tk.Frame(content_frame)
+    welcome_frame.pack(fill = "both" , expand = True)
+
+    response_frame = tk.frame(content_frame)
+    response_frame.pack(fill="both",expand=True)
+
+    history_frame = tk.Frame(content_frame)
+    history_frame.pack(fill="both", expand=True)
 
     window.mainloop()
 main()
