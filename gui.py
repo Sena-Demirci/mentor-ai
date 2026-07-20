de89cb4a24e42d7a20500d08b0a67ba17b935615
Sena Demirci <demircisena206@gmail.com>
2026-07-21 00:42:50 +0300

    Implement Plan screen navigation

--- gui.py
+++ gui.py
@@ -38,17 +38,54 @@ def main():
     goal_label = tk.Label(welcome_frame , text = "What would you like to work on today?")
     goal_label.grid(row=3, column=0)
 
+    def start_screen():
+        clear_screen(welcome_frame)
+
+        plan_button = tk.Button(welcome_frame, text="Plan It", command=plan_mode)
+        plan_button.grid(row=2, column=0, pady=7)
+
+        perspective_button = tk.Button(welcome_frame, text="Different Perspectives")
+        perspective_button.grid(row=3, column=0, pady=7)
+
+        concept_button = tk.Button(welcome_frame, text="Concepts You May Need")
+        concept_button.grid(row=4, column=0, pady=7)
+
+        begin_button = tk.Button(welcome_frame, text="Let's begin")
+        begin_button.grid(row=5, column=0, pady=7)
+
+        start_label = tk.Label(welcome_frame, text = "Where Should I start?")
+        start_label.grid(row = 0, column = 0)
+
+    def plan_mode():
+        clear_screen(welcome_frame)
+
+        plan_it_label = tk.Label(welcome_frame, text="Let's Plan It!")
+        plan_it_label.grid(row=0, column=0, pady=7)
+
+        describe_label = tk.Label(welcome_frame, text = "What are you trying to build?")
+        describe_label.grid(row=1, column=0, pady = 5)
+
+        text_box = tk.Text(welcome_frame,width = 50 , height= 7)
+        text_box.grid(row=2, column=0, pady = 7)
+
+        continue_button = tk.Button(welcome_frame, text = "Continue", command = lambda: continue_mode(text_box))
+        continue_button.grid(row=3, column=0, pady = 7)
+
+    def continue_mode(text_box):
+        print("Continue clicked")
+
+
+
+    def clear_screen(frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
+
+
     goal_entry = tk.Entry(welcome_frame)
     goal_entry.grid(row=4, column=0)
 
-    start_button = tk.Button(welcome_frame, text = "Start")
+    start_button = tk.Button(welcome_frame, text = "Start", command=start_screen)
     start_button.grid(row=5, column=0)
-
-    for widget in welcome_frame.winfo_children():
-        widget.destroy()
-
-    start_label = tk.Label(welcome_frame, text = "Where Should I start?")
-    start_label.grid(row = 0, column = 0)
 
     window.mainloop()
 main()
